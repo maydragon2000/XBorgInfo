f48f612a9d0a3c5e1d9a17417eb3512cb882b767
maydragon2000 <may-dragon2000@outlook.com>
2022-07-07 01:48:56 +0800

    push

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Member1 from '../../assets/profile-1.png'
-import Member2 from '../../assets/profile-2.png'
+import Member2 from '../../assets/profile-4.png'
 import Member3 from '../../assets/profile-3.png'
 import Twitter from '../../assets/Twitter.png'
 import linkedin from '../../assets/linkedin.png'
@@ -11,94 +11,94 @@ import partner2 from '../../assets/partner-1.png'
 // import partner5 from '../../assets/partner-3.png'
 import './team.css'
 const Team = () => {
-  return (
-    <section className="team" id="team">
-        <div className="team-container">
-            <div className="sec-title">
-                <div className="sec-heading" data-aos="fade-left">
-                    <h1>Core Team</h1>
-                    <span></span>
-                </div>  
-            </div>
-            <div className="team-wrapper"  data-aos="fade-up">
-                <div className="member">
-                    <div className="member-img">
-                        {/* <img src={Member1} alt="Member" /> */}
-                        <img src={Member1} alt="member" />
+    return (
+        <section className="team" id="team">
+            <div className="team-container">
+                <div className="sec-title">
+                    <div className="sec-heading" data-aos="fade-left">
+                        <h1>Core Team</h1>
+                        <span></span>
+                    </div>
+                </div>
+                <div className="team-wrapper" data-aos="fade-up">
+                    <div className="member">
+                        <div className="member-img">
+                            {/* <img src={Member1} alt="Member" /> */}
+                            <img src={Member1} alt="member" />
                             <svg>
-                            <path
+                                <path
                                     d="M 87 1 A 86 86 0 1 1 86.9 1"
                                     stroke="#D144F9"
                                     strokeWidth="3"
-                                    fill="none"/>
+                                    fill="none" />
                             </svg>
+                        </div>
+                        <h3>Addy Singh</h3>
+                        <span>Co-Founder and CEO</span>
+                        <a href="https://twitter.com/cryptonite_lad" className='s-link' target='_blank' rel="noopener noreferrer"><img src={Twitter} alt="Twitter" /></a>
                     </div>
-                    <h3>Addy Singh</h3>
-                    <span>Co-Founder and CTO</span>
-                    <a href="https://twitter.com/cryptonite_lad" className='s-link' target='_blank' rel="noopener noreferrer"><img src={Twitter} alt="Twitter" /></a>
-                </div>
-                <div className="member">
-                    <div className="member-img">
-                        <img src={Member2} alt="Member" />
-                         <svg>
-                            <path
+                    <div className="member">
+                        <div className="member-img">
+                            <img src={Member2} alt="Member" />
+                            <svg>
+                                <path
                                     d="M 87 1 A 86 86 0 1 1 86.9 1"
                                     stroke="#D144F9"
                                     strokeWidth="3"
-                                    fill="none"/>
+                                    fill="none" />
                             </svg>
+                        </div>
+                        <h3>Longguo Jin</h3>
+                        <span>Co-Founder and CTO</span>
+                        <a href="https://www.linkedin.com/in/ash-harris-25911426/" className='s-link' target='_blank' rel="noopener noreferrer"><img src={linkedin} alt="Twitter" /></a>
                     </div>
-                    <h3>Ash Harris</h3>
-                    <span>Co-Founder and CEO</span>
-                    <a href="https://www.linkedin.com/in/ash-harris-25911426/" className='s-link' target='_blank' rel="noopener noreferrer"><img src={linkedin} alt="Twitter" /></a>
-                </div>
-                <div className="member">
-                    <div className="member-img">
-                        <img src={Member3} alt="Member" className='img-round' />
-                        <svg>
-                            <path
+                    <div className="member">
+                        <div className="member-img">
+                            <img src={Member3} alt="Member" className='img-round' />
+                            <svg>
+                                <path
                                     d="M 87 1 A 86 86 0 1 1 86.9 1"
                                     stroke="#D144F9"
                                     strokeWidth="3"
-                                    fill="none"/>
+                                    fill="none" />
                             </svg>
+                        </div>
+                        <h3>Netina Beukes</h3>
+                        <span>Creative Director</span>
+                        <a href="https://twitter.com/rat_around" className='s-link' tabIndex='_blank' rel="noopener noreferrer"><img src={Twitter} alt="Twitter" /></a>
                     </div>
-                    <h3>Netina Beukes</h3>
-                    <span>Creative Director</span>
-                    <a href="https://twitter.com/rat_around" className='s-link' tabIndex='_blank' rel="noopener noreferrer"><img src={Twitter} alt="Twitter" /></a>
                 </div>
-            </div>
-            <h1 className='partner-h'>Partners</h1>
-            <div className="partner-container">
-                <div className="partner" data-aos="zoom-in">
-                    <div className="partner-img">
-                        <img src={partner1} alt="partner" />
-                        <svg>
-                            <path
+                <h1 className='partner-h'>Partners</h1>
+                <div className="partner-container">
+                    <div className="partner" data-aos="zoom-in">
+                        <div className="partner-img">
+                            <img src={partner1} alt="partner" />
+                            <svg>
+                                <path
                                     d="M 87 1 A 86 86 0 1 1 86.9 1"
                                     stroke="#D144F9"
                                     strokeWidth="3"
-                                    fill="none"/>
+                                    fill="none" />
                             </svg>
+                        </div>
+                        <h2>Rogue Shift</h2>
+                        {/* <span>Creative Director</span> */}
                     </div>
-                    <h2>Rogue Shift</h2>
-                    {/* <span>Creative Director</span> */}
-                </div>
-                <div className="partner" data-aos="zoom-in">
-                    <div className="partner-img">
-                        <img src={partner2} alt="partner" />
-                        <svg>
-                            <path
+                    <div className="partner" data-aos="zoom-in">
+                        <div className="partner-img">
+                            <img src={partner2} alt="partner" />
+                            <svg>
+                                <path
                                     d="M 87 1 A 86 86 0 1 1 86.9 1"
                                     stroke="#D144F9"
                                     strokeWidth="3"
-                                    fill="none"/>
+                                    fill="none" />
                             </svg>
+                        </div>
+                        <h2>Metatpopit</h2>
+                        {/* <span>Creative Director</span> */}
                     </div>
-                    <h2>Metatpopit</h2>
-                    {/* <span>Creative Director</span> */}
-                </div>
-                {/* <div className="partner" data-aos="zoom-in">
+                    {/* <div className="partner" data-aos="zoom-in">
                     <div className="partner-img">
                         <img src={partner3} alt="partner" />
                         <svg>
@@ -140,10 +140,10 @@ const Team = () => {
                     <h2>Gel Gabalu</h2>
                     <span>Creative Director</span>
                 </div> */}
+                </div>
             </div>
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Team
